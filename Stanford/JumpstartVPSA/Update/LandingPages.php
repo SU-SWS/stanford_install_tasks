6e8f03ae628d2297ea40521d96778b94b5780d6f
John Bickar <jbickar@stanford.edu>
2016-03-23 15:50:06 -0700

    Stub for LandingPage update task

---
+++ Stanford/JumpstartVPSA/Update/LandingPages.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ * Convert VPSA Landing Pages to Stanford Landing Pages.
+ */
+
+namespace Stanford\JumpstartVPSA\Update;
+/**
+ *
+ */
+class LandingPages extends \AbstractUpdateTask {
+
+  protected $description = "Convert VPSA Landing Pages to Stanford Landing Pages";
+
+  /**
+   * Convert VPSA Landing Pages to Stanford Landing Pages.
+   *
+   * @param array $args
+   *   Installation arguments.
+   */
+  public function execute(&$args = array()) {
+  // @todo: this.
+  }
+
+}
