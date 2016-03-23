05c7aacca537b530b14e0655007e181ac82b8c35
Greg Garvey <ggarvey@stanford.edu>
2016-03-23 10:25:19 -0700

    modified menu position rules to use utlility

--- Stanford/JumpstartAcademic/Install/Menu/MenuRules.php
+++ Stanford/JumpstartAcademic/Install/Menu/MenuRules.php
@@ -18,8 +18,9 @@ class MenuRules extends \AbstractInstallTask {
    */
   public function execute(&$args = array()) {
 
-    module_load_include('inc', 'menu_position', 'menu_position.admin');
-
+    //module_load_include('inc', 'menu_position', 'menu_position.admin');
+    # Set menu position default setting to 'mark the rule's parent menu item as being "active".'
+    variable_set('menu_position_active_link_display', 'parent');
     // Define the rules.
     $rules = array();
     $rules[] = array(
@@ -72,43 +73,14 @@ class MenuRules extends \AbstractInstallTask {
       ),
     );
 
+    //foreach ($rules as $mp_rule) {
+    //  $this->insert_menu_rule($mp_rule);
+    //}
     foreach ($rules as $mp_rule) {
-      $this->insert_menu_rule($mp_rule);
+      $rule = new \Stanford\Utility\Install\InsertMenuRule();
+      $rule->insert_menu_rule($mp_rule);
     }
 
-  }
-
-  /**
-  * Create new Menu Position Rule.
-  * @param array $mp_rules A multidimensional array with the following keys:
-  * 'link_title' : Link title in the Main Menu. Assuming depth of 1
-  * 'admin_title' : Administrative title of the Menu Position rule. Human-readable.
-  * 'conditions' : multidimensional array of Menu Position conditions
-  */
-  protected function insert_menu_rule($mp_rule) {
-
-    // Get the mlid of the parent link.
-    $result = db_select('menu_links', 'm')
-    ->fields('m', array('mlid'))
-    ->condition('menu_name', 'main-menu')
-    ->condition('depth', 1)
-    ->condition('link_title', $mp_rule['link_title'])
-    ->execute()
-    ->fetchAssoc();
-
-    $plid = $result['mlid'];
-
-
-    // Create the array to populate the rule.
-    $rule = array(
-      'admin_title' => $mp_rule['admin_title'],
-      'conditions' => $mp_rule['conditions'],
-      'menu_name' => 'main-menu',
-      'plid' => $plid,  // "News" item in main menu. Need to look this up programatically
-    );
-
-    // Calling menu_position_add_rule here because we can assume that no rules have been added.
-    menu_position_add_rule($rule);
   }
 
   /**
