2bbbe648496bd3c9a26d35025c13af853f5c91d6
John Bickar <jbickar+github@stanford.edu>
2016-03-24 16:55:34 -0700

    My hands are just typing words
    http://xkcd.com/1296

--- Stanford/Utility/Install/CreateMenuLinks.php
+++ Stanford/Utility/Install/CreateMenuLinks.php
@@ -8,25 +8,26 @@ namespace Stanford\Utility\Install;
 
 class CreateMenuLinks extends \AbstractInstallTask {
 
+  /**
+   * Create new menu links.
+   * @param array $args A multidimensional array.
+   * Each secondary array has the following keys:
+   *
+   * link_path: (required) The path of the menu item, which should be normalized first by calling drupal_get_normal_path() on it.
+   * link_title: (required) Title to appear in menu for the link.
+   * menu_name: (optional) The machine name of the menu for the link. Defaults to 'navigation'.
+   * weight: (optional) Integer to determine position in menu. Default is 0.
+   * expanded: (optional) Boolean that determines if the item is expanded.
+   * options: (optional) An array of options, see l() for more.
+   * mlid: (optional) Menu link identifier, the primary integer key for each menu link. Can be set to an existing value, or to 0 or NULL to insert a new link.
+   * plid: (optional) The mlid of the parent.
+   * router_path: (optional) The path of the relevant router item.
+   *
+   * See https://api.drupal.org/api/drupal/includes!menu.inc/function/menu_link_save/7
+   */
 
   public function execute(&$args = array()) {
-    /**
-     * Create new menu links.
-     * @param array $args A multidimensional array.
-     * Each secondary array has the following keys:
-     *
-     * link_path: (required) The path of the menu item, which should be normalized first by calling drupal_get_normal_path() on it.
-     * link_title: (required) Title to appear in menu for the link.
-     * menu_name: (optional) The machine name of the menu for the link. Defaults to 'navigation'.
-     * weight: (optional) Integer to determine position in menu. Default is 0.
-     * expanded: (optional) Boolean that determines if the item is expanded.
-     * options: (optional) An array of options, see l() for more.
-     * mlid: (optional) Menu link identifier, the primary integer key for each menu link. Can be set to an existing value, or to 0 or NULL to insert a new link.
-     * plid: (optional) The mlid of the parent.
-     * router_path: (optional) The path of the relevant router item.
-     *
-     * See https://api.drupal.org/api/drupal/includes!menu.inc/function/menu_link_save/7
-     */
+
 
     // Rebuild the menu cache before starting this.
     drupal_static_reset();
