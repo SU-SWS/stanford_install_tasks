69786902bb3a7d80af82837945948d3df629bb00
Shea McKinney <sherakama@gmail.com>
2016-04-08 13:54:23 -0700

    Updates to the sunet user install task

--- Stanford/Jumpstart/Install/User/SunetUser.php
+++ Stanford/Jumpstart/Install/User/SunetUser.php
@@ -18,35 +18,29 @@ class SunetUser extends \AbstractInstallTask {
    */
   public function execute(&$args = array()) {
 
-    $install_state = $args;
-
     // Need this for UI install.
     require_once DRUPAL_ROOT . '/includes/password.inc';
 
-    $sunet = "webservices";
-    $authname = $sunet . '@stanford.edu';
-    $sunet_role = user_role_load_by_name('SUNet User');
-    $owner_role = user_role_load_by_name('site owner');
+    $sunet = $args['forms']['install_configure_form']['stanford_sites_requester_sunetid'];
+    $fullName = $args['forms']['install_configure_form']['stanford_sites_requester_name'];
+    $email = $args['forms']['install_configure_form']['stanford_sites_requester_email'];
 
-    // Change the sunet requester user to site owner.
-    $edit = array();
-    $user3 = user_load_by_mail($authname);
+    $authName = $sunet . '@stanford.edu';
+    $sunetRole = user_role_load_by_name('SUNet User');
+    $ownerRole = user_role_load_by_name('site owner');
 
-    if ($user3) {
-      $roles = array(DRUPAL_AUTHENTICATED_RID => TRUE, $sunet_role->rid => TRUE, $owner_role->rid => TRUE);
-      $edit['roles'] = $roles;
-      $user3 = user_save($user3, $edit);
-    }
-
-    // @TODO: Refactor out once environment stuff is set.
-    if (module_exists('simplesamlphp') && $user3) {
-      user_set_authmaps($user3, array('authname_simplesamlphp_auth' => $authname));
-    }
-
-    if (module_exists("webauth") && $user3) {
-      user_set_authmaps($user3, array('authname_webauth' => $authname));
-    }
-
+     $account = new \stdClass();
+     $account->is_new = TRUE;
+     $account->name = $fullName;
+     $account->pass = user_hash_password(user_password());
+     $account->mail = $email;
+     $account->init = $authname;
+     $account->status = TRUE;
+     $roles = array(DRUPAL_AUTHENTICATED_RID => TRUE, $sunetRole->rid => TRUE, $ownerRole->rid => TRUE);
+     $account->roles = $roles;
+     $account->timezone = variable_get('date_default_timezone', '');
+     $account = user_save($account);
+     user_set_authmaps($account, array('authname_webauth' => $authname));
   }
 
   /**
@@ -64,7 +58,6 @@ class SunetUser extends \AbstractInstallTask {
       "#type" => "textfield",
       "#title" => t("SUNetID."),
       "#description" => t("Requester's SUNetID."),
-      "#default_value" => "",
     );
   
     /**
@@ -75,7 +68,6 @@ class SunetUser extends \AbstractInstallTask {
       "#type" => "textfield",
       "#title" => t("Name."),
       "#description" => t("Requester's preferred name"),
-      "#default_value" => "",
     );
   
   
@@ -87,20 +79,32 @@ class SunetUser extends \AbstractInstallTask {
       "#type" => "textfield",
       "#title" => t("Email."),
       "#description" => t("Requester's preferred email."),
-      "#default_value" => "",
     );
   
-    /**
-     * Set org type: group or dept.
-     * Blank if a personal site.
-     * We will be setting this programatically so we do not want to present it to the user.
-     */
-    $form['stanford_sites_org_type'] = array(
-      "#type" => "textfield",
-      "#title" => t("Group."),
-      "#description" => t("Requester's preferred email."),
-      "#default_value" => "",
+
+  }
+
+  /**
+   * @param $form
+   * @param $form_state
+   */
+  public function validate(&$form, &$form_state) {
+    $values = $form_state['values'];
+    $keys = array(
+      "stanford_sites_requester_sunetid",
+      "stanford_sites_requester_name",
+      "stanford_sites_requester_email",
     );
+
+    foreach ($keys as $key) {
+      if (empty($values[$key])) {
+        form_set_error($key, $key . " must not be empty");
+      }
+    }
+  }
+
+  public function submit(&$form, &$form_state) {
+    // Do something?
   }
 
   /**
