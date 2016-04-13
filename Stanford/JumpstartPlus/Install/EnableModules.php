63627aef0680b08ab647164e0939b810a2cbcc59
Shea McKinney <sherakama@gmail.com>
2016-04-13 13:22:30 -0700

    Added enable modules tasks to JSPLus Install tasks

---
+++ Stanford/JumpstartPlus/Install/EnableModules.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @file
+ * Abstract Task Class.
+ */
+
+namespace Stanford\JumpstartPlus\Install;
+/**
+ *
+ */
+class EnableModules extends \AbstractInstallTask {
+
+  /**
+   * Set the site name.
+   *
+   * @param array $args
+   *   Installation arguments.
+   */
+  public function execute(&$args = array()) {
+    module_enable(array("stanford_jsplus_permissions"));
+  }
+
+}
+
+
+
+
