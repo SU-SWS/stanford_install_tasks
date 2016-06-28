93732e302b05d1056a45c29a63441ca8bb1912e4
John Bickar <jbickar@stanford.edu>
2016-06-28 09:54:12 -0700

    Update JSE to work with block_class-7.x-2.3

--- Stanford/JumpstartEngineering/Install/Block/JSEBlockClasses.php
+++ Stanford/JumpstartEngineering/Install/Block/JSEBlockClasses.php
@@ -19,18 +19,25 @@ class JSEBlockClasses extends \AbstractInstallTask {
   public function execute(&$args = array()) {
 
     // Install default JSE block classes.
-    $fields = array('module', 'delta', 'css_class');
     $values = array(
       array("bean", "jumpstart-small-custom-block", "well"),
       array("bean", "jumpstart-large-custom-block", "well"),
     );
-    // Key all the values.
-    $insert = db_insert('block_class')->fields($fields);
-    foreach ($values as $value) {
-      $db_values = array_combine($fields, $value);
-      $insert->values($db_values);
+    foreach ($values as $k => $value) {
+      // UPDATE block SET (module="bean",delta="social-media",css_class="span4") WHERE module="bean" AND delta="social-media
+      $update = db_update('block')->fields(array('css_class' => $value[2]));
+      $update->condition('module',$value[0]);
+      $update->condition('delta',$value[1]);
+      $update->execute();
     }
-    $insert->execute();
+  }
+  /**
+   *
+   */
+  public function requirements() {
+    return array(
+      'block_class',
+    );
   }
 
 }
