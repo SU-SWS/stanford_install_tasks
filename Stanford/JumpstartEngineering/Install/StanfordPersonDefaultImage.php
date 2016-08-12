1777af519a995f3c098ced3eec4c0b8945c638b4
Caryl Westerberg <cjwest@acm.org>
2016-08-12 13:19:15 -0700

    SOE-1061: Added image .png

--- Stanford/JumpstartEngineering/Install/StanfordPersonDefaultImage.php
+++ Stanford/JumpstartEngineering/Install/StanfordPersonDefaultImage.php
@@ -17,6 +17,18 @@ class StanfordPersonDefaultImage extends \AbstractInstallTask {
    *   Installation arguments.
    */
   public function execute(&$args = array()) {
+    // Get the default banner image
+    $filename = "profile-large-blank.png";
+    $file_path = libraries_get_path('tasks') . '/Stanford/Jumpstart/JumpstartEngineering/img/' . $filename;
+    $image = file_get_contents($file_path);
+
+    //Create a database entry.
+    $file = file_save_data($image, "public://" . $filename, FILE_EXISTS_REPLACE);
+
+    // And save the file id for default_image_ft to use.
+    variable_set('stanford_person_profile_picture', $file->fid);
+
+ /*
     // Setting default image on stanford person using a variable leveraging default_image_ft module.
     $q = db_select('file_managed', 'fm');
     $q->addField('fm', 'fid');
@@ -25,6 +37,7 @@ class StanfordPersonDefaultImage extends \AbstractInstallTask {
     
     // This default image is being pulled in by CAP.
     variable_set('stanford_person_profile_picture', $fid);
+ */
   }
 
 }
