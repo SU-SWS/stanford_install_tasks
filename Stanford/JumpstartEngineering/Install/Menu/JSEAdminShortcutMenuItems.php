3fae1ace956129c374d7b5353157b2d9aa25ac81
Caryl Westerberg <cjwest@acm.org>
2016-09-14 16:34:59 -0700

    SOE-1041: adding Admin Shortcut menu items

---
+++ Stanford/JumpstartEngineering/Install/Menu/JSEAdminShortcutMenuItems.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @file
+ * Create JSE menu items for the Private Pages section
+ */
+
+namespace Stanford\JumpstartEngineering\Install\Menu;
+use Stanford\Utility\Install\CreateMenuLinks;
+
+/**
+ *
+ */
+class JSEAdminShortcutMenuItems extends \AbstractInstallTask {
+
+  /**
+   * Create menu items.
+   *
+   * @param array $args
+   *   Installation arguments.
+   */
+  public function execute(&$args = array()) {
+
+    // Manage meta tags.
+    $items['admin/stanford/jumpstart/shortcuts/site-actions/manage-metatags'] = array(
+      'link_path' => drupal_get_normal_path('admin/config/search/metatags'),
+      'link_title' => 'Manage Meta Tags',
+      'menu_name' => 'menu-admin-shortcuts',
+      'weight' => 30,
+    );
+    // Manage redirects.
+    $items['admin/stanford/jumpstart/shortcuts/site-actions/manage-redirects'] = array(
+      'link_path' => drupal_get_normal_path('admin/config/search/redirect'),
+      'link_title' => 'Manage URL Redirects',
+      'menu_name' => 'menu-admin-shortcuts',
+      'weight' => 32,
+    );
+
+    $linker = new \Stanford\Utility\Install\CreateMenuLinks();
+    $linker->execute($items);
+
+  }
+
+}
