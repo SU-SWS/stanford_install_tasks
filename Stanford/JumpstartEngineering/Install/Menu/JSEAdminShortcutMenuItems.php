e3e63da5b45c19193c58ea1a5dcdd1bc79a8c098
Caryl Westerberg <cjwest@acm.org>
2016-09-14 17:13:33 -0700

    SOE-1041: Updating Admin Shortcut menu items

--- Stanford/JumpstartEngineering/Install/Menu/JSEAdminShortcutMenuItems.php
+++ Stanford/JumpstartEngineering/Install/Menu/JSEAdminShortcutMenuItems.php
@@ -22,10 +22,22 @@ class JSEAdminShortcutMenuItems extends \AbstractInstallTask {
 
     // Manage meta tags.
     $items['admin/stanford/jumpstart/shortcuts/site-actions/manage-metatags'] = array(
+      /*
       'link_path' => drupal_get_normal_path('admin/config/search/metatags'),
       'link_title' => 'Manage Meta Tags',
       'menu_name' => 'menu-admin-shortcuts',
       'weight' => 30,
+      */
+
+      'title' => 'Manage Meta Tags',
+      'description' => 'Manage the meta tags for your site',
+      'page callback' => 'drupal_goto',
+      'page arguments' => array('admin/config/search/metatags'),
+      'access arguments' => array('Administer meta tags'),
+      'type' => MENU_NORMAL_ITEM,
+      'menu_name' => 'menu-admin-shortcuts',
+      'weight' => 30,
+
     );
     // Manage redirects.
     $items['admin/stanford/jumpstart/shortcuts/site-actions/manage-redirects'] = array(
