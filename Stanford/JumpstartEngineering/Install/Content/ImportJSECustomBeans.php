70d05a251908a348aaa2a55da7ce319f8af8aaf8
Caryl Westerberg <cjwest@acm.org>
2016-09-21 10:51:49 -0700

    SOE-1106: change task to just adding custom beans

--- Stanford/JumpstartEngineering/Install/Content/ImportJSECustomBeans.php
+++ Stanford/JumpstartEngineering/Install/Content/ImportJSECustomBeans.php
@@ -4,33 +4,26 @@
  * Import JSE BEANs
  */
 
-use Stanford\Jumpstart\Install\Content\Importer\ImporterFieldProcessorCustomBody as ImporterFieldProcessorCustomBody;
-use Stanford\Jumpstart\Install\Content\Importer\ImporterFieldProcessorFieldSDestinationPublish as ImporterFieldProcessorFieldSDestinationPublish;
+//use Stanford\Jumpstart\Install\Content\Importer\ImporterFieldProcessorCustomBody as ImporterFieldProcessorCustomBody;
+//use Stanford\Jumpstart\Install\Content\Importer\ImporterFieldProcessorCustomFieldSDestinationPublish as ImporterFieldProcessorFieldSDestinationPublish;
 
 namespace Stanford\JumpstartEngineering\Install\Content;
 /**
  *
  */
-class ImportJSEGibbonsBeans extends \AbstractInstallTask {
+class ImportJSECustomBeans extends \AbstractInstallTask {
 
   /**
-   * Import JSE BEANs.
+   * Import Custom BEANs.
    *
    * @param array $args
    *   Installation arguments.
    */
   public function execute(&$args = array()) {
 
-    // @todo: Make this an option on the install form.
     $endpoint = variable_get("stanford_content_server", "https://sites.stanford.edu/jsa-content/jsainstall");
 
     $uuids = array(
-
-      // Jumpstart Home Page Full-Width Banner - No Caption
-      'afb406ad-c08f-4c91-a179-e703a8afc6ca',
-      // Jumpstart Home Page About Block
-      'b66a5774-d0d1-44eb-abda-7aa8ea4eea0e',
-
       // Jumpstart Small Custom Block.
       '7e510af6-c003-402d-91a4-7480dac1484a',
       // Jumpstart Small Custom Block 2.
@@ -41,13 +34,11 @@ class ImportJSEGibbonsBeans extends \AbstractInstallTask {
       '6997adbe-96e6-43f9-b34c-03f6a23197c7',
       // Jumpstart Small Custom Block 5
       '7baae13f-7fc3-489c-bcd7-08698ab08d25',
-
     );
     $importer = new \SitesContentImporter();
     $importer->set_endpoint($endpoint);
     $importer->set_bean_uuids($uuids);
     $importer->import_content_beans();
-
   }
 
   /**
