a65582fdf4b090dec44a32464d0fd0afbb0ffe58
Shea McKinney <sherakama@gmail.com>
2016-10-25 13:56:58 -0700

    Formatting clean up

--- Core/loader.php
+++ Core/loader.php
@@ -8,16 +8,14 @@
  /**
   * Include the files necessary to do the install tasks.
   */
- function itasks_includes() {
-
-   // Things take a long time to run. Lets try to up the limit.
-   ini_set('max_execution_time', 300); //300 seconds = 5 minutes
-
-   require_once dirname(__FILE__) . "/InstallTaskInterface.php";
-   require_once dirname(__FILE__) . "/AbstractTask.php";
-   require_once dirname(__FILE__) . "/AbstractInstallTask.php";
-   require_once dirname(__FILE__) . "/AbstractUpdateTask.php";
-   require_once dirname(__FILE__) . "/TaskEngine.php";
-   require_once dirname(__FILE__) . "/iTasks.php";
-
- }
+function itasks_includes() {
+  // Things take a long time to run. Lets try to up the limit.
+  // 300 seconds = 5 minutes.
+  ini_set('max_execution_time', 300);
+  require_once dirname(__FILE__) . "/InstallTaskInterface.php";
+  require_once dirname(__FILE__) . "/AbstractTask.php";
+  require_once dirname(__FILE__) . "/AbstractInstallTask.php";
+  require_once dirname(__FILE__) . "/AbstractUpdateTask.php";
+  require_once dirname(__FILE__) . "/TaskEngine.php";
+  require_once dirname(__FILE__) . "/iTasks.php";
+}
