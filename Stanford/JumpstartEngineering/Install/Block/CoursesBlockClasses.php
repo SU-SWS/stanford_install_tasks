fbb022f34f1b173fedb4473b1b072ec89203b350
Caryl Westerberg <cjwest@acm.org>
2016-10-24 21:49:11 -0700

    SOE-1307: Added task for courses

---
+++ Stanford/JumpstartEngineering/Install/Block/CoursesBlockClasses.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * Add block classes
+ */
+
+namespace Stanford\JumpstartEngineering\Install\Block;
+/**
+ *
+ */
+class CoursesBlockClasses extends \AbstractInstallTask {
+
+  /**
+   * Set Block Classes.
+   *
+   * @param array $args
+   *   Installation arguments.
+   */
+  public function execute(&$args = array()) {
+
+    // Install default Courses block classes.
+    $values = array(
+      array("views","-exp-courses-search_page","well")
+    );
+
+    foreach ($values as $k => $value) {
+      $update = db_update('block')->fields(array('css_class' => $value[2]));
+      $update->condition('module',$value[0]);
+      $update->condition('delta',$value[1]);
+      $update->execute();
+    }
+  }
+  /**
+   *
+   */
+  public function requirements() {
+    return array(
+      'block_class',
+    );
+  }
+
+}
