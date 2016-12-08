5e58d196f321c54da08a74402ed9622fd3461206
Caryl Westerberg <cjwest@acm.org>
2016-12-07 17:51:36 -0800

    SOE-1506: Put in task to enable the department homepage

---
+++ Stanford/JumpstartEngineering/Install/Layouts/SetDeptHomepage.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @file
+ * Add block classes for courses.
+ */
+
+namespace Stanford\JumpstartEngineering\Install\Layouts;
+
+use \ITasks\AbstractInstallTask;
+
+/**
+ * Set the default department homepage class.
+ */
+class SetHomepage extends AbstractInstallTask {
+
+  /**
+   * Set the homepage.
+   *
+   * @param array $args
+   *   Installation arguments.
+   */
+  public function execute(&$args = array()) {
+
+    // todo: check if a homepage is specified in $args.
+    $homepage = 'stanford_jumpstart_home_gibbons';
+    try {
+      stanford_homepage_set_default_layout($homepage);
+    } catch (\Exception $e) {
+      drush_log($e->getMessage(), 'error');
+      return;
+    }
+
+    drush_log(dt('Enabled homepage: @homepage.', array('@homepage' => $homepage)), 'ok');
+
+  }
+
+  /**
+   * Class requirements.
+   */
+  public function requirements() {
+    return array(
+    );
+  }
+
+}
