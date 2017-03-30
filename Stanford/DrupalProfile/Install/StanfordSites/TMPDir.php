450a00dd280237e973aac4b03bec516055b98cd1
John Bickar <jbickar@stanford.edu>
2017-03-29 19:42:18 -0700

    \!fixup 5PM commits; better doxn and syntax

--- Stanford/DrupalProfile/Install/StanfordSites/TMPDir.php
+++ Stanford/DrupalProfile/Install/StanfordSites/TMPDir.php
@@ -16,18 +16,28 @@ class TMPDir extends AbstractInstallTask {
    *  Installation arguments.
    */
   public function execute(&$args = array()) {
+    // Set the variable from the form input, in case we want to use it later.
     variable_set('stanford_sites_tmpdir', $args['forms']['install_configure_form']['stanford_sites_tmpdir']);
     $tmpdir = variable_get('stanford_sites_tmpdir', file_directory_temp());
+    // Perms on the directory.
     file_prepare_directory($tmpdir, FILE_CREATE_DIRECTORY);
     // system_check_directory() is expecting a $form_element array.
     $element = array();
     $element['#value'] = $tmpdir;
-
     // Check that the temp directory exists; create it if it does not.
     system_check_directory($element);
+    // Actually set the temporary directory path to our desired path.
     variable_set('file_temporary_path', $tmpdir);
-
   }
+
+  /**
+   * Add a form element for gathering the path to the temporary files directory.
+   *
+   * @param $form
+   *   The form.
+   * @param $form_state
+   *   The form state.
+   */
   public function form(&$form, &$form_state) {
     $form['sites']['stanford_sites_tmpdir'] = array(
       '#default_value' => '',
@@ -36,5 +46,5 @@ class TMPDir extends AbstractInstallTask {
       "#description" => t('Enter a path for the temporary files directory (e.g., "sites/default/files/tmp".'),
     );
   }
-}
 
+}
