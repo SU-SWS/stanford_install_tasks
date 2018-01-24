6c6dfcb4f1a489ece1229a863c045673c3d8fb66
John Bickar <jbickar+github@stanford.edu>
2018-01-23 16:38:52 -0800

    BASIC-2410: SUNet User is not getting created properly (#61)
    * BASIC-2410: Fix up variable case.

--- Environment/Sites/Install/User/SunetUser.php
+++ Environment/Sites/Install/User/SunetUser.php
@@ -24,29 +24,32 @@ class SunetUser extends AbstractInstallTask {
     require_once DRUPAL_ROOT . '/includes/password.inc';
 
     $sunet = $args['forms']['install_configure_form']['stanford_sites_requester_sunetid'];
-    $fullName = $args['forms']['install_configure_form']['stanford_sites_requester_name'];
+    $fullname = $args['forms']['install_configure_form']['stanford_sites_requester_name'];
     $email = $args['forms']['install_configure_form']['stanford_sites_requester_email'];
+    $authname = $sunet . '@stanford.edu';
+    $sunetrole = user_role_load_by_name('SUNet User');
+    $ownerrole = user_role_load_by_name('site owner');
 
-    $authName = $sunet . '@stanford.edu';
-    $sunetRole = user_role_load_by_name('SUNet User');
-    $ownerRole = user_role_load_by_name('site owner');
-
-    if (!is_numeric($sunetRole->rid) || !is_numeric($ownerRole->rid)) {
+    if (!is_numeric($sunetrole->rid) || !is_numeric($ownerrole->rid)) {
       throw new \Exception("A role or roles were missing when trying to create a sunet user");
     }
 
     $account = new \stdClass();
     $account->is_new = TRUE;
-    $account->name = $fullName;
+    $account->name = $fullname;
     $account->pass = user_hash_password(user_password());
     $account->mail = $email;
     $account->init = $authname;
     $account->status = TRUE;
-    $roles = array(DRUPAL_AUTHENTICATED_RID => TRUE, $sunetRole->rid => TRUE, $ownerRole->rid => TRUE);
+    $roles = array(
+      DRUPAL_AUTHENTICATED_RID => TRUE,
+      $sunetrole->rid => TRUE,
+      $ownerrole->rid => TRUE,
+    );
     $account->roles = $roles;
     $account->timezone = variable_get('date_default_timezone', '');
-    $account = user_save($account);
 
+    $account = user_save($account);
     user_set_authmaps($account, array('authname_webauth' => $authname));
   }
 
